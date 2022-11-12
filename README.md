672b1f0e3d3bedc1fd70715fbf31dee463d5b300
anh010 <anh010@ucsd.edu>
2022-11-11 19:56:21 -0800

    small typo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Lab 7 - Starter
 Alex Hernandez Perez 
+<br>
 Jinwoong Huh
 
+[published site](https://anh010.github.io/Lab7_Starter/)
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,7 +45,7 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  if ("serviceworker" in navigator){
+  if ("serviceWorker" in navigator){
   // B2. TODO - Listen for the 'load' event on the window object.
     window.addEventListener('load', () => {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
